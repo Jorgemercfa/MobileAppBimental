43561cfaa6144dcc26a2459d23557d1a41d1d738
JorgeLMF <63150944+JorgeLMF@users.noreply.github.com>
2024-11-17 15:56:39 -0500

    add_text_validation

--- bimental_application_1/lib/main.dart
+++ bimental_application_1/lib/main.dart
@@ -36,136 +36,140 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+    final TextEditingController _emailController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
           'Iniciar Sesión',
-          style: TextStyle(color: Color(0xFF1A119B)), // Cambia color del texto
+          style: TextStyle(color: Color(0xFF1A119B)),
         ),
         centerTitle: true,
         elevation: 0,
         backgroundColor: Colors.white,
-        iconTheme: const IconThemeData(
-            color: Color(0xFF1A119B)), // Color de los íconos
+        iconTheme: const IconThemeData(color: Color(0xFF1A119B)),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'BiMental',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF1A119B), // Cambia color del texto principal
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'BiMental',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF1A119B),
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            const CustomTextField(
-              labelText: 'Correo Electronico',
-            ),
-            const SizedBox(height: 20),
-            const CustomTextField(
-              labelText: 'Contraseña',
-              obscureText: true,
-            ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                // Acción al presionar el botón de iniciar sesión
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const HomePage()));
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor:
-                    const Color(0xFF1A119B), // Cambia color de fondo del botón
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Correo Electronico',
+                  labelStyle: TextStyle(color: Color(0xFF1A119B)),
+                  border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                cursorColor: const Color(0xFF1A119B),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, ingrese su correo electrónico';
+                  }
+                  return null;
+                },
               ),
-              child: const Text(
-                'Iniciar Sesión',
-                style:
-                    TextStyle(color: Colors.white), // Color del texto del botón
+              const SizedBox(height: 20),
+              TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Contraseña',
+                  labelStyle: TextStyle(color: Color(0xFF1A119B)),
+                  border: OutlineInputBorder(),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                cursorColor: const Color(0xFF1A119B),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Por favor, ingrese su contraseña';
+                  }
+                  return null;
+                },
               ),
-            ),
-            const SizedBox(height: 10),
-            TextButton(
-              onPressed: () {
-                // Acción al presionar "Olvidé contraseña"
-                Navigator.push(
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Acción al presionar el botón si los campos son válidos
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const HomePage()),
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF1A119B),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
+                child: const Text(
+                  'Iniciar Sesión',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const ResetPasswordPage()));
-              },
-              child: const Text(
-                'Olvidé contraseña',
-                style: TextStyle(
-                    color: Color(0xFF1A119B)), // Cambia color del texto
+                        builder: (context) => const ResetPasswordPage()),
+                  );
+                },
+                child: const Text(
+                  'Olvidé contraseña',
+                  style: TextStyle(color: Color(0xFF1A119B)),
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            TextButton(
-              onPressed: () {
-                // Acción al presionar "Registrar usuario"
-                Navigator.push(
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const RegisterUserPage()));
-              },
-              child: const Text(
-                'Registrar usuario',
-                style: TextStyle(
-                    color: Color(0xFF1A119B)), // Cambia color del texto
+                        builder: (context) => const RegisterUserPage()),
+                  );
+                },
+                child: const Text(
+                  'Registrar usuario',
+                  style: TextStyle(color: Color(0xFF1A119B)),
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            TextButton(
-              onPressed: () {
-                // Acción al presionar "Administrativo"
-                Navigator.push(
+              const SizedBox(height: 10),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context) => const SignInAdmin()));
-              },
-              child: const Text(
-                'Administrativo',
-                style: TextStyle(
-                    color: Color(0xFF1A119B)), // Cambia color del texto
+                        builder: (context) => const SignInAdmin()),
+                  );
+                },
+                child: const Text(
+                  'Administrativo',
+                  style: TextStyle(color: Color(0xFF1A119B)),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-    );
-  }
-}
-
-class CustomTextField extends StatelessWidget {
-  final String labelText;
-  final bool obscureText;
-
-  const CustomTextField({
-    Key? key,
-    required this.labelText,
-    this.obscureText = false,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      obscureText: obscureText,
-      decoration: InputDecoration(
-        labelText: labelText,
-        labelStyle:
-            const TextStyle(color: Color(0xFF1A119B)), // Cambia color del label
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        filled: true,
-        fillColor: Colors.white,
-      ),
-      cursorColor: const Color(0xFF1A119B), // Cambia color del cursor
     );
   }
 }
