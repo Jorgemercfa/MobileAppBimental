0d2bfd7b2def857bd6d1e3c6f582e2c8494c5639
JorgeLMF <63150944+JorgeLMF@users.noreply.github.com>
2025-02-24 16:23:46 -0500

    modified_sign_in_admin

--- bimental_application_1/lib/SignInAdm.dart
+++ bimental_application_1/lib/SignInAdm.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'ForgetPasswordAdmin.dart';
 import 'HomeAdmin.dart';
+import 'AdminRepository.dart';
 
 void main() {
   runApp(const MyApp());
@@ -37,25 +38,35 @@ class SignInAdmin extends StatefulWidget {
 class _SignInAdminState extends State<SignInAdmin> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final GlobalKey<ScaffoldMessengerState> scaffoldKey =
       GlobalKey<ScaffoldMessengerState>();
 
-  // Lista de credenciales permitidas
-  final Map<String, String> validCredentials = {
-    'admin1@bimental.com': '#wrb22ed',
-  };
+  final AdminRepository adminRepository = AdminRepository();
 
   void signIn() {
-    String email = emailController.text.trim();
-    String password = passwordController.text.trim();
+    if (_formKey.currentState!.validate()) {
+      bool adminEncontrado = false;
+      List<Map<String, String>> adminsRegistrados = adminRepository.getAdmins();
 
-    if (validCredentials[email] == password) {
-      Navigator.push(context,
-          MaterialPageRoute(builder: (context) => const HomePageAdmin()));
-    } else {
-      scaffoldKey.currentState?.showSnackBar(
-        const SnackBar(content: Text('Correo o contraseña incorrectos')),
-      );
+      for (var admin in adminsRegistrados) {
+        if (admin['email'] == emailController.text.trim() &&
+            admin['password'] == passwordController.text.trim()) {
+          adminEncontrado = true;
+          break;
+        }
+      }
+
+      if (adminEncontrado) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const HomePageAdmin()),
+        );
+      } else {
+        scaffoldKey.currentState?.showSnackBar(
+          const SnackBar(content: Text('Correo o contraseña incorrectos')),
+        );
+      }
     }
   }
 
@@ -75,68 +86,83 @@ class _SignInAdminState extends State<SignInAdmin> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'BiMental Administrativo',
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFF1A119B),
-              ),
-            ),
-            const SizedBox(height: 20),
-            CustomTextField(
-              controller: emailController,
-              labelText: 'Correo Electrónico',
-            ),
-            const SizedBox(height: 20),
-            CustomTextField(
-              controller: passwordController,
-              labelText: 'Contraseña',
-              obscureText: true,
-            ),
-            const SizedBox(height: 30),
-            SizedBox(
-              width: 250, // Ajusta el ancho del botón
-              child: ElevatedButton(
-                onPressed: signIn,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF1A119B),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                ),
-                child: const Text(
-                  'Iniciar Sesión',
-                  style: TextStyle(color: Colors.white),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'BiMental Administrativo',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF1A119B),
                 ),
               ),
-            ),
-            const SizedBox(height: 10),
-            SizedBox(
-              width: 250, // Ajusta el ancho del botón
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const ForgetPasswordPageAdmin(),
-                    ),
-                  );
+              const SizedBox(height: 20),
+              CustomTextField(
+                controller: emailController,
+                labelText: 'Correo Electrónico',
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Ingrese su correo electrónico';
+                  }
+                  return null;
                 },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF1A119B),
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                ),
-                child: const Text(
-                  'Olvidé contraseña',
-                  style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              CustomTextField(
+                controller: passwordController,
+                labelText: 'Contraseña',
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Ingrese su contraseña';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 30),
+              SizedBox(
+                width: 250,
+                child: ElevatedButton(
+                  onPressed: signIn,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF1A119B),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 20),
+                  ),
+                  child: const Text(
+                    'Iniciar Sesión',
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
-            ),
-          ],
+              const SizedBox(height: 10),
+              SizedBox(
+                width: 250,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const ForgetPasswordPageAdmin(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF1A119B),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 20),
+                  ),
+                  child: const Text(
+                    'Olvidé contraseña',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -147,17 +173,19 @@ class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String labelText;
   final bool obscureText;
+  final String? Function(String?)? validator;
 
   const CustomTextField({
     Key? key,
     required this.controller,
     required this.labelText,
     this.obscureText = false,
+    this.validator,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
       controller: controller,
       obscureText: obscureText,
       decoration: InputDecoration(
@@ -168,6 +196,7 @@ class CustomTextField extends StatelessWidget {
         fillColor: Colors.white,
       ),
       cursorColor: const Color(0xFF1A119B),
+      validator: validator,
     );
   }
 }
