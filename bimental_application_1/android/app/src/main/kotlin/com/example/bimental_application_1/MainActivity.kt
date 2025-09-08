7e6a33db73aa8056dc7a9fcc81e0e26922549277
JorgeLMF <63150944+JorgeLMF@users.noreply.github.com>
2025-09-08 01:44:55 -0500

    prepare_application_to_android

--- bimental_application_1/android/app/src/main/kotlin/com/example/bimental_application_1/MainActivity.kt
+++ bimental_application_1/android/app/src/main/kotlin/com/example/bimental_application_1/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.bimental_application_1
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
