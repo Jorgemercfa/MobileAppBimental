2e5cfa02d625befe91cdec46d2a7581fce712df3
JorgeLMF <63150944+JorgeLMF@users.noreply.github.com>
2025-09-08 00:04:26 -0500

    prepare_application_to_android

---
+++ bimental_application_1/android/app/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.bimental_application_1"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17  // Mejor usar 17 para compatibilidad con Firebase
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "com.example.bimental_application_1"
+        minSdk = 23   // 🔥 Firebase Auth requiere minSdk 23
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
